e167e2c3bc4436ba165cdfbe7bf6e9814198f9ee
Emilie Bronchain <emilie.bronchain28@gmail.com>
2023-06-29 14:48:51 +0200

    fix navbar

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../assets/pictures/Logo.png";
 
 function Navbar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const handleLogin = () => {
+    setIsLoggedIn(!isLoggedIn);
+  };
   return (
     <div className="navbar-containe">
       <nav>
-        <img src={ Logo } className="logo" alt="logo" />
+        <img src={Logo} className="logo" alt="logo" />
         <ul>
           <Link to="/add-article">
             <li>Ajouter un article</li>
@@ -14,9 +18,20 @@ function Navbar() {
           <Link to="/list-article">
             <li>Liste des articles</li>
           </Link>
-          <Link to="/connection">
-            <li className="titleConnection">Connexion</li>
+          <Link to="/list-article">
+            <li>Estimer des articles</li>
           </Link>
+          {!isLoggedIn && (
+            <button
+              className="button-connexion"
+              type="button"
+              onClick={handleLogin}
+            >
+              <Link to="/connection">
+                <li className="titleConnection">Connexion</li>
+              </Link>
+            </button>
+          )}
         </ul>
       </nav>
     </div>
