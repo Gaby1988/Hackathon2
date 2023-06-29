727187c7e16eb182eb2743a51f01315c28afc7e6
EricJKL33 <eric.jkl@icloud.com>
2023-06-29 21:43:01 +0200

    updateContext

--- frontend/src/contexts/IsDesktopContext.jsx
+++ frontend/src/contexts/IsDesktopContext.jsx
@@ -1,14 +1,35 @@
-import React, { createContext, useState, useEffect, useMemo } from "react";
+import React, { createContext, useState, useMemo } from "react";
 import PropTypes from "prop-types";
 
 export const IsDesktopContext = createContext();
 
 export function IsDesktopProvider({ children }) {
   const [isAdmin, setIsAdmin] = useState(false);
+  const [brand, setBrand] = useState("");
+  const [model, setModel] = useState("");
+  const [storageCapacity, setStorageCapacity] = useState("");
+  const [condition, setCondition] = useState("");
+  const [ram, setRam] = useState("");
+  const [price, setPrice] = useState(0);
 
   const contextValueIsDesktop = useMemo(
-    () => ({ isAdmin, setIsAdmin }),
-    [isAdmin]
+    () => ({
+      isAdmin,
+      setIsAdmin,
+      brand,
+      setBrand,
+      model,
+      setModel,
+      storageCapacity,
+      setStorageCapacity,
+      condition,
+      setCondition,
+      ram,
+      setRam,
+      price,
+      setPrice,
+    }),
+    [isAdmin, brand, model, storageCapacity, condition, ram, price]
   );
 
   return (
