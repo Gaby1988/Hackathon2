77b6eddf0fac6a8b9b114a059147c746c7e1a15d
Téo cathelot <t.cathelot.pro@gmail.com>
2023-06-29 12:01:23 +0200

    post router admin

--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -1,7 +1,10 @@
 const express = require("express");
-// 
+//
 const router = express.Router();
 // const tutorialRouter = require("./tutorialRouter");
+const adminRouter = require("./adminRouter");
+
+router.use("/admin", adminRouter);
 
 // router.use("/tutorials", tutorialRouter);
 module.exports = router;
