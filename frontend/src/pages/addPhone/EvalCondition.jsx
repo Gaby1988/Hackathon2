a2353bc70c3eb1ddfeb321c1ae08f3ea3ebb0b4d
Laetitia <laetitia.mdlb@gmail.com>
2023-06-29 23:33:38 +0200

    modifs scss evaluation

--- frontend/src/pages/addPhone/EvalCondition.jsx
+++ frontend/src/pages/addPhone/EvalCondition.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 function EvalCondition({ condition, setCondition, price }) {
   return (
-    <div>
-      <label>État général:</label>
-      <select value={condition} onChange={(e) => setCondition(e.target.value)}>
+    <div className="inputEvalCondition">
+      <label className="titleEvalCondition">État général du téléphone:</label>
+      <select className="inputEvaluationCondition" value={condition} onChange={(e) => setCondition(e.target.value)}>
         <option value="">Sélectionner l'état général</option>
         <option value="Excellent">Excellent état</option>
         <option value="Good">Bon état</option>
         <option value="Mauvais">Mauvais état</option>
         <option value="Other">Autre</option>
       </select>
+      <div>
+      </div>
     </div>
   );
 }
